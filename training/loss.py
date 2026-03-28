32c6af4780e421f9480dece2d48312e9a540b8a9
Yuhang Hu <yuhangh@andrew.cmu.edu>
2026-03-28 11:43:27 -0400

    revise README for whole struction

---
+++ training/loss.py
@@ -0,0 +1,11 @@
+# figure 7 in paper
+
+# 3. Each unique token location (in AR and parallel) should get the same positional embeddings.
+positional_embeddings = get_positional_embeddings(seq_len=seq_len)
+positional_embeddings = positional_embeddings.repeat(2, 1)
+
+loss = model(input_ids, mask=attention_mask, targets=label_ids, positional_embeddings=positional_embeddings)
+    
+loss.backward()
+
+# TODO: implement equation 14
